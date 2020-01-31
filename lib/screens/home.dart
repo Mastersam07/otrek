b042b4f3f43430085b06e6afa76f65a20d96bcfd
Mastersam <abadasamuelosp@gmail.com>
2020-01-30 23:28:17 -0800

    map integration

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:otrek/utils/core.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -13,11 +15,63 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body:
-          Center(), // This trailing comma makes auto-formatting nicer for build methods.
+          Map(), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  GoogleMapController mapController;
+  static const _initialPosition = LatLng(12.92, 77.02);
+  LatLng _lastPosition = _initialPosition;
+  final Set<Marker> _markers = {};
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: _initialPosition,
+            zoom: 10.0,
+          ),
+          onMapCreated: onCreated,
+          myLocationButtonEnabled: true,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          markers: _markers,
+          onCameraMove: _onCameraMove,
+        ),
+        Positioned(
+          top: 40,
+          right: 10,
+          child: FloatingActionButton(
+            onPressed: _onAddMarkerPressed,
+            tooltip: 'add marker',
+            backgroundColor: black,
+            child: Icon(Icons.add_location, color: white,),
+          ),
+        )
+      ],
+    );
+  }
+
+  void onCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {}
 }
